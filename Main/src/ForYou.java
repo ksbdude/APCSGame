6188e92bd8a823220e593f3f48fbf540c18505c8
kevinburns <kevinburns@10.100.7.5>
2016-06-01 22:49:30 -0700

    Revert "Merge origin/master"
    This reverts commit 39af5e9529213a06022bbcf40f5bf53ec5148abc, reversing
    changes made to 20e1d1210082a839d4471f06cbda86f2fa48f91a.

--- Main/src/ForYou.java
+++ Main/src/ForYou.java
@@ -31,24 +31,13 @@ public class ForYou
             }
         }
         
-        //implements RoomStructure class TRANSLATES INTO ROOM ID IN GRAPHICS
-        System.out.println();
-        
-        RoomStructure ja=new RoomStructure(10,10);
-        //int test = ja.getlayout(1);
-        ja.setmapp(haha);
-        haha=ja.getmapp();
-        
+        //implements RoomStructure class
         for(x=0;x<haha.length;x++)
         {
-            System.out.println();
             for(y=0;y<haha[0].length;y++)
             {
-                int temp = ja.getlayout(haha[x][y]);
-                haha[x][y]=temp;
-                System.out.print(haha[x][y] + "\t");
+                //will add later
             }
         }
-        
     }
 }
