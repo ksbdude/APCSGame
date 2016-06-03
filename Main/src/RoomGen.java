bd55448a0c4ae3d8ead554f005b567ff842fcab6
Jonathan <Jonathan@JB1-PC>
2016-06-02 17:54:56 -0700

    RoomGen+RoomStructure combined into RoomGen now. RoomStructure is obsolete. TestRoomGen is just a printout of the 2d array created in RoomGen.

--- Main/src/RoomGen.java
+++ Main/src/RoomGen.java
@@ -5,6 +5,9 @@ public class RoomGen {
 
     public RoomGen(int x, int y) {
         dungen = new int[x][y];
+        generateblock();
+        fillrooms();
+        assignvariation();
     }
 
     public int[][] getmap() {
@@ -154,6 +157,65 @@ public class RoomGen {
                         }
                     }
                 }
+            }
+        }
+    }
+    
+    public int getvariation(int min)
+    {
+        return (int) (Math.random()*10) + min;
+    }
+    
+    /*15 total rooms
+    0     - block room
+    1~40  - single opening
+    41~80 - double opening (also 131~150)
+    81~120 - triple opening
+    121~130- all opening
+    */
+    public int getlayout(int roomstyle)
+    {
+        if(roomstyle==0)
+            return 0;
+        if(roomstyle==1)
+            return getvariation(1);
+        else if(roomstyle==2)
+            return getvariation(11);
+        else if(roomstyle==3)
+            return getvariation(21);
+        else if(roomstyle==4)
+            return getvariation(31);
+        else if(roomstyle==5)
+            return getvariation(41);
+        else if(roomstyle==6)
+            return getvariation(51);
+        else if(roomstyle==7)
+            return getvariation(61);
+        else if(roomstyle==8)
+            return getvariation(71);
+        else if(roomstyle==9)
+            return getvariation(81);
+        else if(roomstyle==10)
+            return getvariation(91);
+        else if(roomstyle==11)
+            return getvariation(101);
+        else if(roomstyle==12)
+            return getvariation(111);
+        else if(roomstyle==13)
+            return getvariation(121);
+        else if(roomstyle==14)
+            return getvariation(131);
+        else
+            return getvariation(141);
+    }
+    
+    public void assignvariation()
+    {
+        for (int x=0;x<dungen.length;x++)
+        {
+            for (int y=0;y<dungen[0].length;y++)
+            {
+                dungen[x][y]=getlayout(dungen[x][y]);
             }
         }
     }
---
+++ Main/src/TestRoomGen.java
@@ -0,0 +1,21 @@
+public class TestRoomGen
+{
+    public static void main (String [] args)
+    {
+        RoomGen ha=new RoomGen(10,10);
+        
+        int[][] haha= new int[10][10];
+        haha=ha.getmap();
+        int x;
+        int y;
+        for(x=0;x<haha.length;x++)
+        {
+            System.out.println();
+            for(y=0;y<haha[0].length;y++)
+            {
+                System.out.print(haha[x][y] + "\t");
+            }
+        }
+        System.out.println();
+    }
+}
